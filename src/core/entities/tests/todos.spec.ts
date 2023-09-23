187995fee18f7af4021ea93d7896b46918aff083
Daniel Alencar <danielmalencar2011@gmail.com>
2023-09-22 22:24:11 -0300

    test: add todo list get all test

--- src/core/entities/tests/todos.spec.ts
+++ src/core/entities/tests/todos.spec.ts
@@ -19,6 +19,25 @@ describe('Todos Class', () => {
     expect(todos.getTodos()).toHaveLength(1)
   })
 
+  test('It should be possible to list all items in the Todo list', () => {
+    const newItem1: TodoItem = {
+      id: 1,
+      title: 'test',
+      completed: false
+    }
+
+    const newItem2: TodoItem = {
+      id: 2,
+      title: 'test2',
+      completed: false
+    }
+
+    const todos = new Todos()
+    todos.addTodo(newItem1)
+    todos.addTodo(newItem2)
+    expect(todos.getTodos()).toHaveLength(2)
+  })
+
   test('it should be possible to find an item by id in the Todo list', () => {
     const newItem: TodoItem = {
       id: 1,
